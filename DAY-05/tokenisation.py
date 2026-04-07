5fe082c44d5e3e58065ef3fa7ec693ed843d6395
SHRISHTI Yadav <147971417+SHRISHTI1414@users.noreply.github.com>
2026-04-07 20:36:50 +0530

    Day 05: Built end-to-end NLP preprocessing pipeline

--- DAY-05/tokenisation.py
+++ DAY-05/tokenisation.py
@@ -10,24 +10,20 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 def preprocess(text):
-     
+    
     text = text.lower()
-
-     
+    
     text = re.sub(r'\d+', '', text)
 
      
     text = text.translate(str.maketrans('', '', string.punctuation))
- # step 4: tokenize
+ 
     tokens = word_tokenize(text)
 
      
     tokens = [word for word in tokens if word not in stop_words]
 
     return tokens
-
-
- 
 news = "Breaking: AI is transforming global markets in 2025, experts say innovation is accelerating rapidly."
 
 processed = preprocess(news)
